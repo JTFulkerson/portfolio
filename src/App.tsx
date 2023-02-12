fa3b98c77f2f09ebe9cfbf1200a1f79f3157fe02
John Fulkerson <johnfulky@mac.com>
2023-02-11 21:02:51 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames';
+
+function App() {
+  return (
+    <div className="h-full items-center bg-black text-white">
+      <h1>React Use Hotkeys</h1>
+    </div>
+  );
+}
+
+export default App;
