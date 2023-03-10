0c71ddf0ea2082d4be3486322aaff50aa3f0c613
John Fulkerson <johnfulky@mac.com>
2023-03-09 19:40:17 -0500

    added animation to all pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,22 @@
+"use client";
 import Image from "next/image";
 import Navbar from "../navbar";
+import { motion } from "framer-motion";
 
 const About = () => {
-    const projects: { imageUrl: string, title: string, description: string }[] = [
+    const projects: { imageUrl: string; title: string; description: string }[] = [
         {
             imageUrl: "/images/ud-sailing.jpg",
-            title: 'University of Delaware Club Sailing Team',
-            description: 'The University of Delaware Club Sailing Team is a collegiate sailing team that represents the University of Delaware in the Middle Atlantic Intercollegiate Sailing Association (MAISA) conference. The team is comprised of seasoned sailor as well as beginners who are passionate about the sport of sailing and committed to representing their school. As a crew member on the team, I have the opportunity to participate in exciting regattas and compete against other top collegiate sailing teams from across the East Coast. These regattas take place throughout the academic year and offer a unique and exhilarating experience for sailors of all skill levels.',
+            title: "University of Delaware Club Sailing Team",
+            description:
+                "The University of Delaware Club Sailing Team is a collegiate sailing team that represents the University of Delaware in the Middle Atlantic Intercollegiate Sailing Association (MAISA) conference. The team is comprised of seasoned sailor as well as beginners who are passionate about the sport of sailing and committed to representing their school. As a crew member on the team, I have the opportunity to participate in exciting regattas and compete against other top collegiate sailing teams from across the East Coast. These regattas take place throughout the academic year and offer a unique and exhilarating experience for sailors of all skill levels.",
         },
     ];
+
+    const variants = {
+        hidden: { opacity: 0, y: -50 },
+        visible: { opacity: 1, y: 0 },
+    };
 
     return (
         <>
@@ -17,9 +25,13 @@ const About = () => {
                 <h1 className="text-xl font-bold mb-8">About Me</h1>
                 <div className="grid grid-cols-1 gap-6">
                     {projects.map((project) => (
-                        <a
+                        <motion.div
                             key={project.title}
                             className="flex rounded-lg overflow-hidden shadow-md"
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.8 }}
+                            variants={variants}
                         >
                             <Image
                                 src={project.imageUrl}
@@ -32,7 +44,7 @@ const About = () => {
                                 <h2 className="text-lg font-bold mb-2">{project.title}</h2>
                                 <p className="text-gray-600">{project.description}</p>
                             </div>
-                        </a>
+                        </motion.div>
                     ))}
                 </div>
             </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import Navbar from "../navbar";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   const projects = [
@@ -26,17 +28,30 @@ const Projects = () => {
     },
   ];
 
+  const variants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1, transition: { duration: 0.6 } },
+    exit: { opacity: 0, transition: { duration: 0.6 } },
+  };
+
   return (
     <>
       <Navbar />
       <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <h1 className="text-xl font-bold mb-8">My Projects</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
           {projects.map((project) => (
-            <a
+            <motion.a
               href={project.link}
               key={project.title}
               className="block rounded-lg overflow-hidden shadow-md hover:shadow-lg  hover:text-blue-500"
+              variants={variants}
             >
               <Image
                 src={project.imageUrl}
@@ -50,9 +65,9 @@ const Projects = () => {
                 <h2 className="text-lg font-bold mb-2">{project.title}</h2>
                 <p className="text-base text-gray-600">{project.description}</p>
               </div>
-            </a>
+            </motion.a>
           ))}
-        </div>
+        </motion.div>
       </div>
     </>
   );
