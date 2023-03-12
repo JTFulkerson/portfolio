52d65b0881f80b1e2b4fbacdc88bef30e940fd4d
John Fulkerson <johnfulky@mac.com>
2023-03-11 21:01:35 -0500

    changed about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,55 +1,120 @@
-"use client"
+"use client";
+import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../navbar";
 import { motion } from "framer-motion";
 
 const About = () => {
-    const projects: {
-        imageUrl: string;
-        title: string;
-        description: string;
-    }[] = [
-            {
-                imageUrl: "/images/ud-sailing.jpg",
-                title: "University of Delaware Club Sailing Team",
-                description:
-                    "The University of Delaware Club Sailing Team is a collegiate sailing team that represents the University of Delaware in the Middle Atlantic Intercollegiate Sailing Association (MAISA) conference. The team is comprised of seasoned sailor as well as beginners who are passionate about the sport of sailing and committed to representing their school. As a crew member on the team, I have the opportunity to participate in exciting regattas and compete against other top collegiate sailing teams from across the East Coast. These regattas take place throughout the academic year and offer a unique and exhilarating experience for sailors of all skill levels.",
-            },
-        ];
+    const hobbies = [
+        { name: "Mountain Biking", icon: "🚵" },
+        { name: "Sailing", icon: "⛵️" },
+        { name: "Scuba Diving", icon: "🤿" },
+        { name: "Music", icon: "🎶" },
+    ];
+
+    const skills = [
+        { name: "Java", icon: "☕️", proficiency: "Intermediate" },
+        { name: "Python", icon: "🐍", proficiency: "Intermediate" },
+        { name: "C", icon: "🅒", proficiency: "Beginner" },
+        { name: "HTML", icon: "🌐", proficiency: "Intermediate" },
+        { name: "CSS", icon: "💅", proficiency: "Intermediate" },
+        { name: "Node.js", icon: "🟢", proficiency: "Beginner" },
+        { name: "React", icon: "⚛️", proficiency: "Beginner" },
+    ];
+
+    const certifications = [
+        { name: "PADI Rescue Diver", icon: "🏊‍♂️" },
+        { name: "PADI Emergency First Responder", icon: "🩹" },
+        { name: "IYT International Crew", icon: "⛵️" },
+        { name: "Marine Communications (VHF-SRC)", icon: "📡" },
+    ];
 
     const variants = {
-        hidden: { opacity: 0, y: -50 },
-        visible: { opacity: 1, y: 0 },
+        initial: { opacity: 0 },
+        animate: { opacity: 1, transition: { duration: 0.6 } },
+        exit: { opacity: 0, transition: { duration: 0.6 } },
     };
 
     return (
         <>
             <Navbar />
             <div className="max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-xl font-bold mb-8">About Me</h1>
-                <div className="grid grid-cols-1 gap-6">
-                    {projects.map((project) => (
-                        <motion.div
-                            key={project.title}
-                            className="flex flex-col md:flex-row rounded-lg overflow-hidden shadow-md"
-                            initial={{ opacity: 0, y: 50 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.8 }}
-                            variants={variants}
-                        >
-                            <Image
-                                src={project.imageUrl}
-                                alt={project.title}
-                                className="md:w-1/3 object-cover"
-                                width={1000}
-                                height={1000}
-                            />
-                            <div className="p-6 md:w-2/3">
-                                <h2 className="text-lg font-bold mb-2">{project.title}</h2>
-                                <p className="text-gray-600">{project.description}</p>
-                            </div>
-                        </motion.div>
-                    ))}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                    <motion.div
+                        className="p-4 shadow-lg rounded-lg"
+                        variants={variants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        key="introduction"
+                    >
+                        <h1 className="text-2xl font-bold mb-4">Introduction</h1>
+                        <p className="text-lg text-gray-600">
+                            Hi, I&apos;m John Fulkerson, a freshman studying Computer Science with a concentration in Systems and Networks at the University of Delaware. I&apos;m from McLean, Virginia, just minutes outside of Washington, D.C.
+                        </p>
+                    </motion.div>
+                    <motion.div
+                        className="p-4 shadow-lg rounded-lg"
+                        variants={variants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        key="hobbies"
+                    >
+                        <h1 className="text-2xl font-bold mb-4">Hobbies</h1>
+                        <div className="grid grid-cols-2 gap-4">
+                            {hobbies.map((hobby, index) => (
+                                <div className="flex items-center" key={`hobby-${index}`}>
+                                    <span className="text-2xl mr-2">
+                                        {hobby.icon}
+                                    </span>
+                                    <span className="text-lg">{hobby.name}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="p-4 shadow-lg rounded-lg"
+                        variants={variants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        key="skills"
+                    >
+                        <h1 className="text-2xl font-bold mb-4">Skills</h1>
+                        <div className="grid grid-cols-2 gap-4">
+                            {skills.map((skill, index) => (
+                                <div className="flex items-center" key={`skill-${index}`}>
+                                    <span className="text-2xl mr-2">
+                                        {skill.icon}
+                                    </span>
+                                    <span className="text-lg">{skill.name}</span>
+                                    <span className="text-lg font-semibold ml-2">{skill.proficiency}</span>
+                                </div>
+                            ))}
+
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="p-4 shadow-lg rounded-lg"
+                        variants={variants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        key="certifications"
+                    >
+                        <h1 className="text-2xl font-bold mb-4">Certifications</h1>
+                        <div className="grid grid-cols-2 gap-4">
+                            {certifications.map((certification, index) => (
+                                <div className="flex items-center" key={`certification-${index}`}>
+                                    <span className="text-2xl mr-2">
+                                        {certification.icon}
+                                    </span>
+                                    <span className="text-lg">{certification.name}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </motion.div>
                 </div>
             </div>
         </>
