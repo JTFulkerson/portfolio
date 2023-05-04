2c4ae23f078328229481d0f77080b07687615a18
John Fulkerson <johnfulky@mac.com>
2023-05-04 15:13:21 -0400

    Added view details button to projects

--- app/projects/head.tsx
+++ app/projects/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
             <title>John Fulkerson | Projects
             </title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
-            <meta name="description" content="Projects created by John Fulkerson" />
+            <meta name="description" content="All of John Fulkerson's Projects" />
             <link rel="icon" href="/favicon.ico" />
         </>
     )
